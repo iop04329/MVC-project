7148d67657efee5a40fc65e19dce6acb70bf9606
DESKTOP-2C4H3JO\tsai <iop04329@gmail.com>
2022-03-03 00:01:08 +0800

    模型繫結 MarkDown用法

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # MVC-project
 My first MVC project
 
+`MarkDown用法:https://ithelp.ithome.com.tw/articles/10203758`  
 - [x] 1. First Install Visual studio 2019
 - [x] 2. choose install ASP.Net與網頁程式開發&.Net桌面開發  個別元件:LINQ to SQL
 - [x] 3. 影片學習1-1 Url:https://www.youtube.com/watch?v=9spaHik87-A
 - [x] 4. 影片教學1-2 Url:https://www.youtube.com/watch?v=BFkIFg1iFLo  
 DTC
-- [x] 5. 影片教學ASP NET MVC待辦事項CRUD(一) 
+- [x] 1. 影片教學ASP NET MVC待辦事項CRUD(一) 
 Url:https://www.youtube.com/watch?v=GhwVSzJZoHk&list=PLygCabSM5MspocL_cCQtf27E3aYojmdgn
-- [x] 6. 延續第五點遇到EntityFramework問題(6.2.0 -> 6.1.3) Nuget套件先移除6.2.0 在去安裝6.1.3  
+- [x] 2. 延續第五點遇到EntityFramework問題(6.2.0 -> 6.1.3) Nuget套件先移除6.2.0 在去安裝6.1.3  
 Nuget上網搜尋問題解決:https://dotblogs.com.tw/OldNick/2017/03/24/162927
-- [x] 7. 影片教學ASP NET MVC待辦事項CRUD(二) 
+- [x] 3. 影片教學ASP NET MVC待辦事項CRUD(二) 
 Url:https://www.youtube.com/watch?v=GhwVSzJZoHk&list=PLygCabSM5MspocL_cCQtf27E3aYojmdgn
-- [x] 8. 影片教學ASP.NET MVC 控制器與動作方法
+- [x] 4. 影片教學ASP.NET MVC 控制器與動作方法
 Url:https://www.youtube.com/watch?v=li9sMIdNEho&list=PLygCabSM5MspocL_cCQtf27E3aYojmdgn&index=4
--- WebPractice/WebApplication1/Controllers/MVCController.cs
+++ WebPractice/WebApplication1/Controllers/MVCController.cs
@@ -9,13 +9,26 @@ namespace WebApplication1.Controllers
     public class MVCController : Controller
     {
         // GET: MVC
-        public ActionResult Index()
+        public ActionResult Index()  // data傳輸練習
         {
             ViewData["A"] = "測試ViewData傳輸資料到前端";
             ViewBag.B = "測試ViewBag傳輸資料到前端";
             TempData["C"] = "測試TempData傳輸資料到前端";
             return View();
         }
+        public ActionResult Index2()  // 模型繫結
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index2(string PId,string PName,int Price)  // 模型繫結
+        {
+            ViewBag.PId = PId;
+            ViewBag.PName = PName;
+            ViewBag.Price = Price;
+            return View();
+        }
+
         public string ActionTest()  //練習控制器與動作方法
         {
             //return "OK";
@@ -28,6 +41,14 @@ namespace WebApplication1.Controllers
                     show += string.Format("<img src ='../image/{0}.png' width='150'>", i);
             }
             return show;
+        }
+        public string ShowImage(int index)  //練習控制器與動作方法 (帶入參數&帶入參數)
+        {
+            //return "OK";
+            string []text = new string[] { "Book1","Book2","Book3","Poland"};
+            string img = string.Format("<p align='center'><img src='../image/{0}.png'><br>{1}</p>", index, text[index]);
+            
+            return img;
         }
     }
 }
