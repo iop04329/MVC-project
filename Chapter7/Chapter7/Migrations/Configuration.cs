9a9fd427960bbdf86087f8f022ac49b36da10e70
EFCTW\chiachun.tsai <iop04329@gmail.com>
2022-07-13 18:20:23 +0800

    Chapter7

---
+++ Chapter7/Chapter7/Migrations/Configuration.cs
@@ -0,0 +1,37 @@
+﻿namespace Chapter7.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Chapter7.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Chapter7.Models.CmsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Chapter7.Models.CmsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            context.Employees.AddOrUpdate(
+                x => x.Id,
+                    new Employee { Id = 1, Name = "David", Mobile = "0933-152667", Email = "david@gmail.com", Department = "總經理室", Title = "CEO" },
+                    new Employee { Id = 2, Name = "Mary", Mobile = "0938-456889", Email = "mary@gmail.com", Department = "人事部", Title = "管理師" },
+                    new Employee { Id = 3, Name = "Joe", Mobile = "0925-331225", Email = "joe@gmail.com", Department = "財務部", Title = "經理" },
+                    new Employee { Id = 4, Name = "Mark", Mobile = "0935-863991", Email = "mark@gmail.com", Department = "業務部", Title = "業務員" },
+                    new Employee { Id = 5, Name = "Rose", Mobile = "0987-335668", Email = "rose@gmail.com", Department = "資訊部", Title = "工程師" }
+                );
+
+            context.Registers.AddOrUpdate(
+                x => x.Id,
+                    new Register { Id = 1, Name = "奚江華", Nickname = "聖殿祭司", Password = "myPassword*", Email = "dotnetcool@gmail.com", City = 4, Gender = 1, Commutermode = "1", Comment = "Nothing", Terms = true }
+                );
+        }
+    }
+}
